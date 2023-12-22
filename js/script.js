6c2ede35cb892d841d9d12c7f9c9cea004f67f52
windy <kjyy2028@gmail.com>
2023-12-22 10:52:10 +0900

    loading 스크립트로 넣기

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 $(function () {
   // 대상을 변수에 저장
   const $window = $(window);
-  const $loading = $('.loading');
+  const $body = $('body');
+  const $loading = ` <div class="loading">
+  <div class="lds-circle"><div></div></div>
+</div>`;
+
+  // body에 마지막 부분에 집어넣기
+  $body.append($loading);
+  const $target = $('.loading');
 
   // 로딩이 완료되면 (load)
   $window.on('load', function () {
     // 너무빨리 사라지므로 조금 있다가 사라지게끔
     // setTimeout(동작, 시간)
     setTimeout(function () {
-      $loading.fadeOut();
+      $target.fadeOut();
     }, 1000);
   });
 });
